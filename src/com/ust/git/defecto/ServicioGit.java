6856d1d1d19032b4ef6b7a1d1f7ea8fade7255f0
xjossanc <jose.sanchez.tamborero@itbo-ericsson.com>
2021-03-25 10:40:16 +0100

    Comienzo del proyecto

---
+++ src/com/ust/git/defecto/ServicioGit.java
@@ -0,0 +1,12 @@
+package com.ust.git.defecto;
+
+public class ServicioGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Repositorio Git creado desde entorno local");
+		
+	}
+
+}
