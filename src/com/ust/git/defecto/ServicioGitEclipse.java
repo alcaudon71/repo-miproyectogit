805766ba9dd3468163eeb0b106bc3c4b12e6ecad
xjossanc <jose.sanchez.tamborero@itbo-ericsson.com>
2021-03-25 10:50:06 +0100

    Commit realizado desde Eclipse

---
+++ src/com/ust/git/defecto/ServicioGitEclipse.java
@@ -0,0 +1,12 @@
+package com.ust.git.defecto;
+
+public class ServicioGitEclipse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Commit realizado desde Eclipse");
+		
+	}
+
+}
